c33d53418714900b5b5a17d6d2651616b6dcbd83
Vipul Vadhe <133830996+Vipul7620@users.noreply.github.com>
2023-09-05 10:04:57 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <div class="header">
+  
+  <h2>BANK OF MIT
+  </h2>
+</div> 
+      <a class="navbar-brand" href="index.php"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">HOME</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">HISTORY</a>
+              </li>
+          </div>
+       </nav>
